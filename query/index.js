48ed4336fa64ac0f3d5d6174b287122e0976986a
PurthaShaariyaar <purtha.shaariyaar@ontariotechu.net>
2024-01-03 03:48:18 +0600

    Finished client side. All components and their functionalities completed.

--- query/index.js
+++ query/index.js
@@ -9,18 +9,21 @@ const app = express();
 // 3. Use bodyParser middleware to parse JSON in the request body
 app.use(bodyParser.json());
 
-// 4. Storing all courses locally including descriptions essentially an object
+// 4. Bypass CORS error
+app.use(cors());
+
+// 5. Storing all courses locally including descriptions essentially an object
 const courses = {};
 
 /**
- * Route handler to send all courses
+ * 6. Route handler to send all courses
  */
 app.get('/courses', (req, res) => {
   res.send(courses);
 });
 
 /**
- * Route handler to handle all POST events
+ * 7. Route handler to handle all POST events
  * Destructure the data from the request body: type & data
  * Run 2 type checks -> associate the id & the posts & insert in the destructured data
  */
@@ -46,7 +49,7 @@ app.post('/events', (req, res) => {
   res.send({});
 });
 
-// Start the server & listen on port 4002
+// 8. Start the server & listen on port 4002
 app.listen(4002, () => {
   console.log('Listening on port 4002...');
 });
